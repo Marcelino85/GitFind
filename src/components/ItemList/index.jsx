0d9ac53f5a92c3eff9719e17b43349e33b3066b2
Marcelino <marcelino85albuquerque@gmail.com>
2023-12-26 13:55:01 -0300

    Conclusão do projeto GitFind

---
+++ src/components/ItemList/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.css'
+
+function ItemList({title, description}) {
+  return (
+    <div className='ItemList'>
+        <strong>{title}</strong>
+        <p>{description}</p>
+        <hr />
+    
+    </div>
+    );  
+}
+
+export default ItemList
