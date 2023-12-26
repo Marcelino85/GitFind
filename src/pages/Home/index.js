9bb4f9b2aaa8956c0f39c0e6e04fa21dcddaf586
Marcelino <marcelino85albuquerque@gmail.com>
2023-12-25 23:40:11 -0300

    Desenvolvimento do projeto Gitfind

---
+++ src/pages/Home/index.js
@@ -0,0 +1,35 @@
+import { Header } from '../../components/Header';
+import background from '../../assets/background.png';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div className='conteudo'>
+        <img src={background} className="background" alt='background app'/> 
+        <div className ='infor'>
+              <div>
+                <input name ='usuario' placeholder ='@username'/>
+                <button>Buscar</button>
+              </div>
+
+              <div className="perfil" >
+                <img src="https://avatars.githubusercontent.com/u/106674529?v=4" className="profile"  alt='imagem perfil'/>
+                <div>
+                  <h3>Marcelino Albuquerque</h3>
+                  <span>@Marcelino85</span>
+                  <p>Description</p>
+                </div>
+              </div>
+
+              <hr/>
+        </div>
+      </div>
+     
+      
+    </div>
+  );
+}
+
+export default App;
